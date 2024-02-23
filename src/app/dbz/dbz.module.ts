2f213105c2d7e5a4491e78a07f5bbfea92735355
Nicolas <nmunoz@dynamosol.com.ar>
2024-02-23 13:44:38 -0300

    Primer Commit

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/dbz/components/list/list.component';
+import { AddCharacterComponent } from './components/dbz/components/add-character/add-character.component';
+
+@NgModule({
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [MainPageComponent],
+})
+export class DbzModule {}
